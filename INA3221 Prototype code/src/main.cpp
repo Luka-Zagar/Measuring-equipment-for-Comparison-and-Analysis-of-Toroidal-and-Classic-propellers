0758acf9bdc068d3f9aa24cafa6599e9ac09d20a
Luka Zagar <lukazagar64@gmail.com>
2023-07-22 10:16:45 +0200

    Library upload

--- INA3221 Prototype code/src/main.cpp
+++ INA3221 Prototype code/src/main.cpp
@@ -1,18 +1,41 @@
-#include <Arduino.h>
+// SDL_Arduino_INA3221 Library Test Code
+// Version 1.0
+// Luka Žagar   22 July 2023
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Arduino.h>
+#include <Wire.h>
+#include <SDL_Arduino_INA3221.h>
+
+#define CHANNEL_1 1
+
+SDL_Arduino_INA3221 ina3221(INA3221_ADDRESS, 0.004F);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("Measuring voltage and current with INA3221");
+  ina3221.begin();
+
+  int MID = ina3221.getManufID();
+  Serial.println(MID,HEX);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  float shuntvoltage1 = 0;
+  float busvoltage1 = 0;
+  float current_mA1 = 0;
+  float loadvoltage1 = 0;
+  float power1 = 0;
+  
+  busvoltage1 = ina3221.getBusVoltage_V(CHANNEL_1);
+  shuntvoltage1 = ina3221.getShuntVoltage_mV(CHANNEL_1);
+  current_mA1 = ina3221.getCurrent_mA(CHANNEL_1);  // - means the battery is charging, + that it is discharging
+  
+  loadvoltage1 = busvoltage1 + (shuntvoltage1 / 1000);
+  power1 = loadvoltage1 * (current_mA1 / 1000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print("Bus Voltage: "); Serial.print(busvoltage1); Serial.print(" V    ");
+  Serial.print("Load Voltage: "); Serial.print(loadvoltage1); Serial.print(" V    ");
+  Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage1); Serial.print(" mV   ");
+  Serial.print("Current: "); Serial.print(current_mA1); Serial.print(" mA   ");
+  Serial.print("Power: "); Serial.print(power1); Serial.println(" W");
 }
