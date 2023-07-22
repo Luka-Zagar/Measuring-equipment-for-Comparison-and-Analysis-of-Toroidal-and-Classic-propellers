e57cdf9ba68b83bba06c72f4e41050e952503004
Luka Zagar <lukazagar64@gmail.com>
2023-07-22 18:09:24 +0200

    PWM to Power upload

---
+++ PWM to power Prototype code/src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+#define SIGNALPIN_1 2
+
+int SignalValue;
+int MotorPower;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(SIGNALPIN_1, INPUT);
+}
+
+void loop() {
+  SignalValue = pulseIn(SIGNALPIN_1, HIGH);
+  MotorPower = map(SignalValue, 1000, 2000, 0, 100);
+  
+  Serial.print("PWM Signal: ");   Serial.print(SignalValue);  Serial.print("\t");
+  Serial.print("Power in %: ");   Serial.println(MotorPower);
+}
